5f9309baf031f28d745d07f96be2f42f94617a97
iii <miselrkba@gmail.com>
2020-11-14 19:27:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import UserTable from "./UserTable";
+import AddUserForm from './AddUserForm'
 
-function App() {
+const App = () => {
+  const initialBookState = {
+    title: "",
+    available: false,
+  };
+  const usersData = [
+    { id: 1, name: "Tania", username: "floppydiskette" },
+    { id: 2, name: "Craig", username: "siliconeidolon" },
+    { id: 3, name: "Ben", username: "benisphere" },
+  ];
+  const [users, setUsers] = useState(usersData);
+  const [book, setBook] = useState(usersData);
+
+  const updateBook = (book) => {
+    setBook({ title: book.title, available: book.available });
+  };
+
+  const addUser = (user) => {
+    user.id = users.length + 1
+    setUsers([...users, user])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+          <AddUserForm addUser={addUser} />
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users={users} />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
